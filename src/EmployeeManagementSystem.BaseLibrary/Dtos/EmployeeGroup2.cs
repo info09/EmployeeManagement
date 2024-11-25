4296760cd1374933e86c376ff080181cd6348488
huytq@ics-p.vn <huytq@ics-p.vn>
2024-11-25 10:09:31 +0700

    update code

--- src/EmployeeManagementSystem.BaseLibrary/Dtos/EmployeeGroup2.cs
+++ src/EmployeeManagementSystem.BaseLibrary/Dtos/EmployeeGroup2.cs
@@ -1,10 +1,16 @@
-﻿namespace EmployeeManagementSystem.BaseLibrary.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagementSystem.BaseLibrary.Dtos
 {
     public class EmployeeGroup2
     {
-        public string JobName { get; set; }
+        [Required]
+        public string? JobName { get; set; }
+        [Required, Range(1, 9999, ErrorMessage = "You must select branch")]
         public int BranchId { get; set; }
+        [Required, Range(1, 9999, ErrorMessage = "You must select town")]
         public int TownId { get; set; }
+        [Required]
         public string? Other { get; set; }
     }
 }
